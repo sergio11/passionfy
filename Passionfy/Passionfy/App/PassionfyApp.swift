25ba535ce18173f6587a55986a13ba85e3fb6e08
Sergio Sánchez <sss4esob@gmail.com>
2024-11-27 18:46:13 +0100

    Implement initial project structure

---
+++ Passionfy/Passionfy/App/PassionfyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassionfyApp.swift
+//  Passionfy
+//
+//  Created by Sergio Sánchez Sánchez on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PassionfyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
