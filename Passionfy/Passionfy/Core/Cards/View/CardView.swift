1c3a64886867b6e867ec4861deda4bca7f90a2b9
Sergio Sánchez <sss4esob@gmail.com>
2024-11-27 19:20:17 +0100

    Implement several improvements in card view

--- Passionfy/Passionfy/Core/Cards/View/CardView.swift
+++ Passionfy/Passionfy/Core/Cards/View/CardView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct CardView: View {
     
+    @State private var xOffset: CGFloat = 0
+    @State private var degrees: Double = 0
+    
     var body: some View {
         ZStack(alignment: .bottom) {
             Image("example")
@@ -19,11 +22,39 @@ struct CardView: View {
         }
         .frame(width: cardWidth, height: cardHeight)
         .clipShape(RoundedRectangle(cornerRadius: 10))
+        .offset(x: xOffset)
+        .rotationEffect(.degrees(degrees))
+        .animation(.spring(), value: xOffset)
+        .gesture(
+            DragGesture()
+                .onChanged(onDragChanged).onEnded(onDragEnded)
+        )
+    }
+}
+
+private extension CardView {
+    
+    func onDragChanged(_ value: _ChangedGesture<DragGesture>.Value) {
+        xOffset = value.translation.width
+        degrees = Double(value.translation.width / 25)
+    }
+    
+    func onDragEnded(_ value: _ChangedGesture<DragGesture>.Value) {
+        let width = value.translation.width
+        if abs(width) <= abs(screenCutOff) {
+            xOffset = 0
+            degrees = 0
+        }
     }
 }
 
 
 private extension CardView {
+    
+    var screenCutOff: CGFloat {
+        (UIScreen.main.bounds.width / 2) * 0.8
+    }
+    
     var cardWidth: CGFloat {
         UIScreen.main.bounds.width - 20
     }
