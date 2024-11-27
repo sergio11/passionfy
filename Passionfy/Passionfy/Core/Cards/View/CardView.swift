3d7fcf039546696e00a1d1743e3d6fe3de049d20
Sergio Sánchez <sss4esob@gmail.com>
2024-11-27 19:56:11 +0100

    Implement several improvements in Card view

--- Passionfy/Passionfy/Core/Cards/View/CardView.swift
+++ Passionfy/Passionfy/Core/Cards/View/CardView.swift
@@ -11,16 +11,30 @@ struct CardView: View {
     
     @State private var xOffset: CGFloat = 0
     @State private var degrees: Double = 0
+    @State private var currentImageIndex = 0
+    
+    @State private var mockImages = [
+        "example",
+        "example",
+        "example"
+    ]
     
     var body: some View {
         ZStack(alignment: .bottom) {
-            Image("example")
-                .resizable()
-                .scaledToFit()
-            
+            ZStack(alignment: .top) {
+                Image(mockImages[currentImageIndex])
+                    .resizable()
+                    .scaledToFit()
+                    .overlay {
+                        ImageScrollingOverlay(currentImageIndex: $currentImageIndex, imageCount: mockImages.count)
+                    }
+                SwipeActionIndicatorView(xOffset: $xOffset)
+            }
+        
             UserInfoView()
+                .padding(.horizontal)
         }
-        .frame(width: cardWidth, height: cardHeight)
+        .frame(width: SizeConstants.cardWidth, height: SizeConstants.cardHeight)
         .clipShape(RoundedRectangle(cornerRadius: 10))
         .offset(x: xOffset)
         .rotationEffect(.degrees(degrees))
@@ -41,26 +55,10 @@ private extension CardView {
     
     func onDragEnded(_ value: _ChangedGesture<DragGesture>.Value) {
         let width = value.translation.width
-        if abs(width) <= abs(screenCutOff) {
+        if abs(width) <= abs(SizeConstants.screenCutOff) {
             xOffset = 0
             degrees = 0
         }
-    }
-}
-
-
-private extension CardView {
-    
-    var screenCutOff: CGFloat {
-        (UIScreen.main.bounds.width / 2) * 0.8
-    }
-    
-    var cardWidth: CGFloat {
-        UIScreen.main.bounds.width - 20
-    }
-    
-    var cardHeight: CGFloat {
-        UIScreen.main.bounds.height / 1.45
     }
 }
 
