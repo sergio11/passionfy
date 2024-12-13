3470c73b30b874faca6ef835b14f946c960103a5
Sergio Sánchez <sss4esob@gmail.com>
2024-12-13 21:00:45 +0100

    refactoring onboarding screens by creating common onboarding step view

--- Passionfy/Passionfy/View/Onboarding/CreateAccount/CreateAccountView.swift
+++ Passionfy/Passionfy/View/Onboarding/CreateAccount/CreateAccountView.swift
@@ -13,29 +13,39 @@ struct CreateAccountView: View {
     @StateObject var viewModel = CreateAccountViewModel()
     
     var body: some View {
-        Group {
+        ZStack {
             switch viewModel.accountFlowStep {
             case .username:
                 EnterNameView()
+                    .transition(.slide)
             case .birthdate:
                 EnterAgeView()
+                    .transition(.slide)
             case .gender:
                 SelectGenderView()
+                    .transition(.slide)
             case .interest:
                 SelectInterestView()
+                    .transition(.slide)
             case .preference:
                 SelectPreferencesView()
+                    .transition(.slide)
             case .phoneNumber:
                 EnterPhoneNumberView()
+                    .transition(.slide)
             case .otp:
                 ValidateOTPView()
+                    .transition(.slide)
             case .completed:
                 AccountCreatedView(isAccountCreated: $isAccountCreated)
+                    .transition(.slide)
             }
         }
+        .animation(.easeInOut(duration: 0.5), value: viewModel.accountFlowStep)
         .environmentObject(viewModel)
     }
 }
+
 
 struct CreateAccountView_Previews: PreviewProvider {
     static var previews: some View {
